0e28e3f421bc06014b485bdc9961a98f663190ac
leo-vaporwing <leo.vaporwing@gmail.com>
2025-11-21 15:12:45 +0100

    added main user to foundry group

--- hosts/rpi-400/default.nix
+++ hosts/rpi-400/default.nix
@@ -65,7 +65,7 @@
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.${username} = {
     isNormalUser = true;
-    extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+    extraGroups = [ "wheel" "foundry" ]; # Enable ‘sudo’ for the user.
     packages = with pkgs; [
       fastfetch
       neovim
